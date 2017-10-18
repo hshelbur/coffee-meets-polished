d35dbf129c5a6601ffd555691dbd0601a90c9de3
hshelbur <howard.shelburne@gmail.com>
2017-10-17 21:03:34 -0400

    article 31 added, updated instagram

--- src/layout/Sidebar.js
+++ src/layout/Sidebar.js
@@ -27,15 +27,15 @@ class InstagramPhotos extends Component {
         <section> 
           <h1><a href="https://www.instagram.com/coffeemeetspolished/" target="_blank" rel="noopener noreferrer">CMP on Instagram</a></h1>
           <ul>
+            <li><a href="https://www.instagram.com/p/BaW9Q-XFw_7/" target="_blank" rel="noopener noreferrer"><img src="https://scontent-iad3-1.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/22580327_125952768067184_7697977297457905664_n.jpg" alt="Richard Branson" /></a></li>
+            <li><a href="https://www.instagram.com/p/BaTxAe2FIDz/" target="_blank" rel="noopener noreferrer"><img src="https://scontent-iad3-1.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/22499763_289451918238672_3694360791609245696_n.jpg" alt="Big Fall Leaf" /></a></li>
+            <li><a href="https://www.instagram.com/p/BaRuYECFMRb/" target="_blank" rel="noopener noreferrer"><img src="https://scontent-iad3-1.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/22427572_2106132106280923_6722017546481958912_n.jpg" alt="Where You Are" /></a></li>
             <li><a href="https://www.instagram.com/p/BaPS_yml1R6/" target="_blank" rel="noopener noreferrer"><img src="https://scontent-iad3-1.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/22500348_136877886938874_361555161635618816_n.jpg" alt="Reality Check" /></a></li>
             <li><a href="https://www.instagram.com/p/BaMBBEnlVga/" target="_blank" rel="noopener noreferrer"><img src="https://scontent-iad3-1.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/22427529_170090663543537_5889260689578524672_n.jpg" alt="Make Days Count" /></a></li>
             <li><a href="https://www.instagram.com/p/BaKLnsPFAzh/" target="_blank" rel="noopener noreferrer"><img src="https://scontent-iad3-1.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/22352361_2045797352315083_1799916232774778880_n.jpg" alt="Michelle Strong Men" /></a></li>           
             <li><a href="https://www.instagram.com/p/BaHK-O-lKNr/" target="_blank" rel="noopener noreferrer"><img src="https://scontent-iad3-1.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/22352407_137592243542284_1427426539670077440_n.jpg" alt="I Am the Greatest" /></a></li>
             <li><a href="https://www.instagram.com/p/BaEs3sCF381/" target="_blank" rel="noopener noreferrer"><img src="https://scontent-iad3-1.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/22352173_349286845529708_625477401576472576_n.jpg" alt="Edge of Gender" /></a></li>
             <li><a href="https://www.instagram.com/p/BaB7IA4FbN0/" target="_blank" rel="noopener noreferrer"><img src="https://scontent-iad3-1.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/22351970_476647489387388_8772986252774342656_n.jpg" alt="Madonna" /></a></li>
-            <li><a href="https://www.instagram.com/p/BZ6qvgUFUpp/" target="_blank" rel="noopener noreferrer"><img src="https://scontent-iad3-1.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/22159434_238603010003879_5479841282819883008_n.jpg" alt="Flu Vaccine" /></a></li>
-            <li><a href="https://www.instagram.com/p/BZ6lAeCFfTg/" target="_blank" rel="noopener noreferrer"><img src="https://scontent-iad3-1.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/22277626_1771983466434171_5570951283063390208_n.jpg" alt="Weapons and Birth Control" /></a></li> 
-            <li><a href="https://www.instagram.com/p/BZ3cbTmlWQh/" target="_blank" rel="noopener noreferrer"><img src="https://scontent-iad3-1.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/22070964_931319280368166_7214971520232194048_n.jpg" alt="Cam Newton" /></a></li>
           </ul>  
         </section>
       </div>
