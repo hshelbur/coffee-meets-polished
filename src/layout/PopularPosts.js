2462c08bbde6937fe7cb4f0f17b8e1933017a6a5
Erin Turingan <eturingan@Erins-MacBook-Pro-2.local>
2018-01-14 12:17:22 -0500

    updated article ids and popular posts

--- src/layout/PopularPosts.js
+++ src/layout/PopularPosts.js
@@ -9,12 +9,13 @@ class PopularPosts extends Component {
 				<h3>Popular Posts</h3>
 				<nav>
 					<ul>
-						<a href="/articles/58"><img src="/images/hopefulfor2018.png" alt="HopefulFor2018" /></a>
-						<a href="/articles/57"><img src="/images/favoritepodcastsof2017.png" alt="Favorite Podcasts of 2017" /></a>
-						<a href="/articles/56"><img src="/images/19before2019.png" alt="19 Before 2019" /></a>
-						<a href="/articles/55"><img src="/images/highsof2017preview.png" alt="Highs of 2017" /></a>
-						<a href="/articles/54"><img src="/images/lovedindecember.png" alt="5 Things I Loved in December" /></a>
-						<a href="/articles/53"><img src="/images/mentalhealthresolutions.png" alt="Mental Health Resolutions" /></a>
+						<a href="/articles/topics-to-normalize-in-2018-periods"><img src="/images/normalizedconvoperiods.png" alt="topics-to-normalize-in-2018-periods" /></a>
+						<a href="/articles/hopeful-for-in-2018"><img src="/images/hopefulfor2018.png" alt="hopeful-for-in-2018" /></a>
+						<a href="/articles/favorite-podcasts-of-2017"><img src="/images/favoritepodcastsof2017.png" alt="favorite-podcasts-of-2017" /></a>
+						<a href="/articles/19-before-2019-a-goal-setting-challenge"><img src="/images/19before2019.png" alt="19-before-2019-a-goal-setting-challenge" /></a>
+						<a href="/articles/highs-and-lows-of-2017"><img src="/images/highsof2017preview.png" alt="highs-and-lows-of-2017" /></a>
+						<a href="/articles/5-things-i-loved-in-december-2017"><img src="/images/lovedindecember.png" alt="5-things-i-loved-in-december-2017" /></a>
+						<a href="/articles/mental-health-resolutions-for-2018"><img src="/images/mentalhealthresolutions.png" alt="mental-health-resolutions-for-2018" /></a>
 					</ul>
 				</nav>
 			</div>
