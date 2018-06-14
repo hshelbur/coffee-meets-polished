db2829d04c2b89e5742a05d48b6850cd8ff3b948
LtCupcakes541 <vinnywilliams13@gmail.com>
2018-06-13 20:24:58 -0400

    make entire instaLink box clickable link

--- src/pages/InstagramLandingPage.js
+++ src/pages/InstagramLandingPage.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import InstagramLink from '../layout/InstagramLink.js';
 
+  
 const InstagramLandingPage = props => (
-	
       <main className="link-page">
-      
+     
         {props.instagramLinks.map(function(link){
-          return <InstagramLink key={link.id} title={link.title} />
+          return <a href = {`articles/${link.id}`}><InstagramLink key={link.id} title={link.title} /></a>
         })}
+
+        
 
       </main>
 )
